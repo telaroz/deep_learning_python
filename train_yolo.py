e14f5a923fcac50f4d3412d3c33acd1e928094b2
Valery <telaroz@gmail.com>
2025-04-30 17:14:46 +0200

    Change to model version 11

--- train_yolo.py
+++ train_yolo.py
@@ -2,9 +2,9 @@ from ultralytics import YOLO
 
 data_yaml_path = 'datasets/data.yaml'
 
-# Choose the model architecture (e.g., yolov8n.pt, yolov8s.pt, yolov8m.pt, yolov8x.pt)
+# Choose the model architecture (e.g., yolo11n.pt, yolo11s.pt, yolo11m.pt, yolo11l.pt, yolo11x.pt) Sizes here: https://docs.ultralytics.com/models/yolo11/#supported-tasks-and-modes
 
-model_name = 'yolov8n.pt' # Using nano version for faster training --> 8x is bigger/better
+model_name = 'yolo11n.pt' # Using nano version for faster training --> 11x is bigger/better. 
 
 model = YOLO(model_name)
 
